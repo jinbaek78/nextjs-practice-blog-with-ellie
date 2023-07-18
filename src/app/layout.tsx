c7f29885d6f6706ff21b420a367c769cc815948c
jinbaek <jinmb0602@gmail.com>
2023-07-18 16:04:11 +0900

    feat: add page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import Header from '@/components/Header';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
+import Footer from '@/components/Footer';
 
 const sans = Open_Sans({ subsets: ['latin'] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={sans.className}>
-      <body className={sans.className}>{children}</body>
+      <body className=" flex flex-col w-full max-w-screen-2xl mx-auto">
+        <Header />
+        <main className="bg-slate-500 grow">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
