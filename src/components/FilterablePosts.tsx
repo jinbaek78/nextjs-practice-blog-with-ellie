ea5dfc4ec1fa94791681c17270793050745e8586
jinbaek <jinmb0602@gmail.com>
2023-07-19 15:11:59 +0900

    feat: make filterablePosts components

---
+++ src/components/FilterablePosts.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Post } from '@/service/posts';
+import { useState } from 'react';
+import PostsGrid from './PostsGrid';
+import Categories from './Categories';
+
+const ALL_POSTS = 'All Posts';
+
+type Props = {
+  posts: Post[];
+  categories: string[];
+};
+export default function FilterablePosts({ posts, categories }: Props) {
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filtered =
+    selected === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category === selected);
+  return (
+    <section className="flex">
+      <PostsGrid posts={filtered} />
+      <Categories
+        categories={[ALL_POSTS, ...categories]}
+        selected={selected}
+        onClick={setSelected}
+      />
+    </section>
+  );
+}
