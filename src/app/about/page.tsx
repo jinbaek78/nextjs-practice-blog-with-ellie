39071146932e84dba36c6c2e853ac07854d3cb02
jinbaek <jinmb0602@gmail.com>
2023-07-20 17:54:24 +0900

    feat: add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,41 @@
+import Hero from '@/components/Hero';
+
 export default function AboutPage() {
-  return <p>About!</p>;
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <Hero />
+      <article className="bg-gray-100 w-full shadow-lg my-7">
+        <div className="flex flex-col text-center p-5">
+          <h1 className="text-2xl font-bold mb-2">Who AM I?</h1>
+          <p className="text-lg ">
+            a fullstack developer who loves development
+          </p>
+          <p className="text-lg">
+            {' '}
+            {"I'am developing involved human and design"}
+          </p>
+        </div>
+        <div className="flex flex-col text-center p-5">
+          <h1 className="text-2xl font-bold mb-2">Carrer</h1>
+          <p className="text-lg ">
+            a fullstack developer who loves development
+          </p>
+          <p className="text-lg">
+            {' '}
+            {"I'am developing involved human and design"}
+          </p>
+        </div>
+        <div className="flex flex-col text-center p-5">
+          <h1 className="text-2xl font-bold mb-2">Skills</h1>
+          <p className="text-lg ">
+            a fullstack developer who loves development
+          </p>
+          <p className="text-lg">
+            {' '}
+            {"I'am developing involved human and design"}
+          </p>
+        </div>
+      </article>
+    </section>
+  );
 }
